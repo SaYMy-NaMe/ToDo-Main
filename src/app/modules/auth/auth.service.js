f2266d8d584953a898e6429f3d35bfd0e3dd7c7e
ashraf <ashrafurrahman42069@gmail.com>
2023-07-24 02:20:54 +0600

    Verify Auth Token middlewares and Login

--- src/app/modules/auth/auth.service.js
+++ src/app/modules/auth/auth.service.js
@@ -1,10 +1,55 @@
+const config = require("../../../config");
+const pool = require("../../../db");
+const ApiError = require("../../../errors/ApiError");
+var jwt = require("jsonwebtoken");
+
 const createUserInDB = async (payload) => {
-  const createdUser = console.log(payload);
+  const { name, gender, designation, phoneNumber, password } = payload;
+
+  const query =
+    "INSERT INTO users (name, gender, designation, phoneNumber, password, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, current_timestamp, current_timestamp) RETURNING id, name, gender, designation, phoneNumber, created_at, updated_at";
+  const values = [name, gender, designation, phoneNumber, password];
+
+  const createdUser = (await pool.query(query, values)).rows[0];
   return createdUser;
+};
+
+const loginUser = async (payload) => {
+  const { phoneNumber, password } = payload;
+  const query = "SELECT * FROM users WHERE phoneNumber = $1";
+  const values = [phoneNumber];
+
+  const result = await pool.query(query, values);
+  const user = result.rows[0];
+
+  if (user) {
+    if (user.password === password) {
+      const { id, name, phonenumber } = user;
+
+      const accessToken = jwt.sign(
+        {
+          id,
+          name,
+          phonenumber,
+        },
+        config.jwt.secret,
+        { expiresIn: config.jwt.expires_in }
+      );
+
+      return {
+        accessToken,
+      };
+    } else {
+      throw new ApiError(401, "Invalid password");
+    }
+  } else {
+    throw new ApiError(404, "User does not exist");
+  }
 };
 
 const authService = {
   createUserInDB,
+  loginUser,
 };
 
 module.exports = authService;
